30b61b15933ed83f9e38b129eda0f5a0ac40c671
valabji <valabji@gmail.com>
2021-06-14 06:39:04 +0200

    first commit

---
+++ constants/Fonts.js
@@ -0,0 +1,9 @@
+export default {
+    regular_ar: "Tajawal_400Regular",
+    bold_ar: "Tajawal_700Bold",
+    black_ar: "Tajawal_900Black",
+
+    regular_en: "Montserrat_400Regular",
+    bold_en: "Montserrat_700Bold",
+    black_en: "Montserrat_900Black"
+};
